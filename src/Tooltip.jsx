29cb70f8515e4354267490ab9221a00519a8c42f
yinm <yinm.cminor9@gmail.com>
2018-07-05 08:15:54 +0900

    Add base

---
+++ src/Tooltip.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Trigger from 'rc-trigger'
+import { placements } from './placements'
+import Content from './Content'
+
+class Tooltip extends Component {
+  static propTypes = {
+    trigger: PropTypes.any,
+    children: PropTypes.any,
+    defaultVisible: PropTypes.bool,
+    visible: PropTypes.bool,
+    placement: PropTypes.string,
+    transitionName: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+    ]),
+    animation: PropTypes.any,
+    onVisibleChange: PropTypes.func,
+    afterVisibleChange: PropTypes.func,
+    overlay: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.func,
+    ]).isRequired,
+    overlayStyle: PropTypes.object,
+    overlayClassName: PropTypes.string,
+    prefixCls: PropTypes.string,
+    mouseEnterDelay: PropTypes.number,
+    mouseLeaveDelay: PropTypes.number,
+    getTooltipContainer: PropTypes.func,
+    destroyTooltipOnHide: PropTypes.bool,
+    align: PropTypes.object,
+    arrowContent: PropTypes.any,
+    id: PropTypes.string,
+  }
+
+  static defaultProps = {
+    prefixCls: 'rc-tooltip',
+    mouseEnterDelay: 0,
+    destroyTooltipOnHide: false,
+    mouseLeaveDelay: 0.1,
+    align: {},
+    placements: 'right',
+    trigger: ['hover'],
+    arrowContent: null,
+  }
+}
+
+export default Tooltip
