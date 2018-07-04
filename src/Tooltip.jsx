6d7347b90b8c57b17bb5010b6fefe1ef3e938425
yinm <yinm.cminor9@gmail.com>
2018-07-05 08:26:57 +0900

    Add render

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -68,6 +68,59 @@ class Tooltip extends Component {
   saveTrigger = (node) => {
     this.trigger = node
   }
+
+  render() {
+    const {
+      overlayClassName,
+      trigger,
+      mouseEnterDelay,
+      mouseLeaveDelay,
+      overlayStyle,
+      prefixCls,
+      children,
+      onVisibleChange,
+      afterVisibleChange,
+      transitionName,
+      animation,
+      placement,
+      align,
+      destroyTooltipOnHide,
+      defaultVisible,
+      getTooltipContainer,
+      ...restProps,
+    } = this.props
+
+    const extraProps = { ...restProps }
+    if ('visible' in this.props) {
+      extraProps.popupVisible = this.props.visible
+    }
+
+    return (
+      <Trigger
+        popupClassName={overlayClassName}
+        ref={this.saveTrigger}
+        prefixCls={prefixCls}
+        popup={this.getPopupElement}
+        action={trigger}
+        builtinPlacements={placements}
+        popupPlacement={placement}
+        popupAlign={align}
+        getPopupContainer={getTooltipContainer}
+        onPopupVisibleChange={onVisibleChange}
+        afterPopupVisibleChange={afterVisibleChange}
+        popupTransitionName={transitionName}
+        popupAnimation={animation}
+        defaultPopupVisible={defaultVisible}
+        destroyPopupOnHide={destroyTooltipOnHide}
+        mouseLeaveDelay={mouseLeaveDelay}
+        popupStyle={overlayStyle}
+        mouseEnterDelay={mouseEnterDelay}
+        {...extraProps}
+      >
+        {children}
+      </Trigger>
+    )
+  }
 }
 
 export default Tooltip
